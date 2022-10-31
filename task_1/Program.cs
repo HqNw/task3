3b8440d88dd325026b479fddba3aba7aaf5ea19b
HqNw <redalabeb166@gmail.com>
2022-10-31 18:57:37 +0200

    hi

---
+++ task_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the value of x:");
+            int16 x = int16.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of y:");
+            int16 y = int16.Parse(Console.ReadLine());            
+
+            Console.WriteLine("Enter the value of z:");
+            int16 z = int16.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
